a9680b5ab272100d802778f95a457d459c1a2ba1
jame <breakfastblast@gmail.com>
2021-10-30 01:47:37 -0400

    Added gameboard factory + tests

--- src/_tests/gameboard.test.js
+++ src/_tests/gameboard.test.js
@@ -1,7 +1,61 @@
 import Gameboard from "../factory/gameboard";
+import Ship from "../factory/ship";
 
 describe("Creating a gameboard", () => {
 	it("generates a gameboard", () => {
 		expect(new Gameboard().board.length).toBe(100);
+	});
+});
+
+describe("Gameboard functions", () => {
+	let gameboard;
+	let carrier;
+	beforeEach(() => {
+		gameboard = new Gameboard();
+		carrier = Ship.newShipFromType("carrier", 3, false);
+	});
+	it("can get all the coordinates from a valid ship", () => {
+		let carrier = Ship.newShipFromType("carrier", 3, false);
+		expect(gameboard.getPlacementCoordinates(carrier)).toStrictEqual([
+			3, 4, 5, 6, 7,
+		]);
+	});
+	it("can get coordinates of a ship oriented across the y-axis", () => {
+		carrier.yAxis = true;
+		expect(gameboard.getPlacementCoordinates(carrier)).toStrictEqual([
+			3, 13, 23, 33, 43,
+		]);
+	});
+	it("doesn't get coordinates for an out-of-bounds ship", () => {
+		carrier.position = -1;
+		expect(gameboard.getPlacementCoordinates(carrier)).toBe(false);
+	});
+	it("doesn't get coordinates for a ship that overlaps with another ship", () => {
+		gameboard.board[4].hasShip = true;
+		expect(gameboard.getPlacementCoordinates(carrier)).toBe(false);
+	});
+});
+
+describe("Placing a ship", () => {
+	let gameboard;
+	let carrier;
+	beforeEach(() => {
+		gameboard = new Gameboard();
+		carrier = Ship.newShipFromType("carrier", 3, false);
+	});
+	it("can place a valid ship", () => {
+		gameboard.addShip(carrier);
+		expect(gameboard.board[3].hasShip).toBe(true);
+	});
+	it("doesn't place out-of-bounds ships", () => {
+		carrier.position = -1;
+		gameboard.addShip(carrier);
+		expect(gameboard.board[3].hasShip).toBe(false);
+	});
+	it("doesn't place ships that overlap", () => {
+		const submarine = Ship.newShipFromType("submarine", 4, false);
+		gameboard.addShip(submarine);
+		gameboard.addShip(carrier);
+		expect(gameboard.board[99].hasShip).toBe(false);
 	});
 });
--- src/factory/gameboard.js
+++ src/factory/gameboard.js
@@ -1,6 +1,43 @@
 class Gameboard {
 	constructor() {
-		this.board = Array(10 * 10).fill({ hasShip: false, isShot: false });
+		this.board = Array(10 * 10)
+			.fill()
+			.map((square) => ({ hasShip: false, isShot: false }));
+	}
+
+	getPlacementCoordinates(ship) {
+		//Get "coordinates" for each ship segment based on it's main position
+		let coordinates = [];
+		for (let i = 0; i < ship.length; i++) {
+			let nextCoordinate = ship.position;
+			if (ship.yAxis === true) {
+				nextCoordinate += 10 * i;
+			} else {
+				nextCoordinate += 1 * i;
+			}
+
+			//Check for illegal positions (out of bounds, or overlapping other ships)
+			if (
+				this.board[nextCoordinate] === undefined ||
+				this.board[nextCoordinate].hasShip === true ||
+				nextCoordinate > this.board.length ||
+				(ship.yAxis === false && nextCoordinate % 10 === 0)
+			) {
+				return false;
+			}
+			coordinates.push(nextCoordinate);
+		}
+		return coordinates;
+	}
+
+	addShip(ship) {
+		let placementCoordinates = this.getPlacementCoordinates(ship);
+		if (placementCoordinates === false) return false; //Don't allow bad placements
+
+		for (let i = 0; i < placementCoordinates.length; i++) {
+			this.board[placementCoordinates[i]].hasShip = true;
+		}
+		return true;
 	}
 }
 
